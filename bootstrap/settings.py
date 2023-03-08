cf85a7b8b2f72a3bf4eebde38d42d9e6cff1a409
maheshprojects33 <65019077+maheshprojects33@users.noreply.github.com>
2023-03-08 23:36:09 +0545

    Update settings.py

--- bootstrap/settings.py
+++ bootstrap/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4o!m!_r%b44%&b)=ug2tbj7e$_2=58=$rs%ax1g3+*fqu547$l'
+SECRET_KEY = '4o!m!_r$rs%ax1g3+*fqu547$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
